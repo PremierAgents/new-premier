ecb2e71f79f9ebf6c19e11e80fec44af7ff43eff
Wekesa Buyahi <blackscure@Wekesas-MacBook-Pro.local>
2022-05-01 12:28:37 +0300

    modal enlarged

--- resources/views/navigation/nav.blade.php
+++ resources/views/navigation/nav.blade.php
@@ -19,7 +19,6 @@
           <li class="nav-item">
             <a class="nav-link" href="{{url('/House')}}"id="nav-white">House</a>
           </li>
-
       </ul>
 
       <ul class="navbar-nav ">
@@ -46,7 +45,7 @@
 
     <!-- Modal content -->
     <center>
-    <div class="modal-content" style="width:30%">
+    <div class="modal-content" id="login-form">
       <div class="modal-header">
         <span class="close">&times;</span>
       </div>
@@ -120,7 +119,6 @@
     </div>
     </div>
 </center>
-
   </div>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
